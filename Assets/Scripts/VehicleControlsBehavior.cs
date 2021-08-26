66ee9e095c1540aca048415461b3d118f04e3955
Ben Brownlie <benjibspam@gmail.com>
2021-08-26 15:35:37 -0500

    Created small physics test with brick walls

---
+++ Assets/Scripts/VehicleControlsBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControlsBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Rigidbody _rigidbody;
+
+    public void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * _speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * _speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * _speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+    }
+}
