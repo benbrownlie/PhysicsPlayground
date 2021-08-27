e4f1feefa671b64227da2e80f6c5f2b7832c29f8
Ben Brownlie <benjibspam@gmail.com>
2021-08-27 16:01:06 -0500

    Began working on PhysicsVehicle script
    Created a script called "PhysicsVehicleControlsBehavior". The purpose is for the player to enter simple WASD keys causing velocity to be applied to the vehicles motors.

---
+++ Assets/Scripts/PhysicsVehicleControlsBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsVehicleControlsBehavior : MonoBehaviour
+{
+    public HingeJoint frontleft;
+    public HingeJoint frontright;
+    public HingeJoint backleft;
+    public HingeJoint backright;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Front left wheel
+            JointMotor motor1 = frontleft.motor;
+            motor1.targetVelocity = 500;
+            frontleft.motor = motor1;
+
+            //Front right wheel
+            JointMotor motor2 = frontright.motor;
+            motor2.targetVelocity = 500;
+            frontright.motor = motor2;
+
+            //Back left wheel
+            JointMotor motor3 = backleft.motor;
+            motor3.targetVelocity = 500;
+            backleft.motor = motor3;
+
+            //Back right wheel
+            JointMotor motor4 = backright.motor;
+            motor4.targetVelocity = 500;
+            backright.motor = motor4;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Front left wheel
+            JointMotor motor1 = frontleft.motor;
+            motor1.targetVelocity = -500;
+            frontleft.motor = motor1;
+
+            //Front right wheel
+            JointMotor motor2 = frontright.motor;
+            motor2.targetVelocity = -500;
+            frontright.motor = motor2;
+
+            //Back left wheel
+            JointMotor motor3 = backleft.motor;
+            motor3.targetVelocity = -500;
+            backleft.motor = motor3;
+
+            //Back right wheel
+            JointMotor motor4 = backright.motor;
+            motor4.targetVelocity = -500;
+            backright.motor = motor4;
+        }
+    }
+}
